2979ea6ef1f4f4f3daa5624a5dde8249644e65ed
hardik4116y <93897125+hardik4116y@users.noreply.github.com>
2021-11-08 09:53:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function multiply(p1,p2)
+{
+  return p1*p2
+}
+const person = {
+ firstName: "John",
+ lastName: "Doe",
+ age: 50,
+  eyeColor: "blue"
+};
+document.getElementById('demo').innerHTML=(person.eyeColor)
+function cuts()
+{
+  var name='Hardik';
+  document.getElementById('id').innerHTML=(name.slice(1,5))
+  
+}
